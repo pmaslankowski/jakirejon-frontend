2e5eaf83a6db32935378ecb64f470feb2e13204a
Piotr Maślankowski <pmaslankowski@advatech.pl>
2020-11-19 20:58:13 +0000

    cleanup

--- src/search/components/SearchForm.js
+++ src/search/components/SearchForm.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-// TODO: absoulute paths
 import { fetchMatchingStreets } from './../search-api';
 import { parseAddress } from './../address';
 import Autocomplete from './Autocomplete';
